689cd26bc1f6434d6b99191dd31c46f6df7ad99f
Rico Kahler <ricokahler@me.com>
2020-12-29 04:42:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a simple mirror of `https://oembed.com/providers.json` via Vercel.
+
+It's cached via `stale-while-revalidate` with a maxage of 86400.
+
+TL;DR you can hit a good amount and it'll refresh automatically every 24 hours.
---
+++ api/providers.ts
@@ -0,0 +1,25 @@
+import createFetch from '@vercel/fetch';
+import { NowApiHandler } from '@vercel/node';
+
+const fetch = createFetch();
+
+const handler: NowApiHandler = async (req, res) => {
+  if (req.method !== 'GET') {
+    res.status(404).end();
+    return;
+  }
+
+  const response = await fetch('https://oembed.com/providers.json', {
+    headers: { Accept: 'application/json' },
+  });
+
+  const providers = await response.json();
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // this doesn't get sent to the client. it's actually a pragma for vercel
+  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate');
+
+  res.json(providers);
+};
+
+export default handler;
